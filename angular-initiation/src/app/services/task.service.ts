faa7b41b57e619835de660bd4a6e4ccb2758f9f1
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-09-19 11:34:16 +0100

    switched to using json-server for requesting data (tasks)

--- angular-initiation/src/app/services/task.service.ts
+++ angular-initiation/src/app/services/task.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { TASKS } from 'src/app/mockTasks';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../Task';
 import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    // return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
