10937de1c6b0abc52bee2a566999801030bceab5
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-09-20 14:44:07 +0100

    added routes /about and home page. Add new task button hide when switching route

--- angular-initiation/src/app/app.module.ts
+++ angular-initiation/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {HttpClientModule } from '@angular/common/http';
-import{ FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,7 +11,13 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskFormComponent } from './components/add-task-form/add-task-form.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+const appRoutes: Routes = [
+  {path: '',component: TasksComponent,},
+  { path: 'about', component: AboutComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,15 +25,18 @@ import { AddTaskFormComponent } from './components/add-task-form/add-task-form.c
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskFormComponent
+    AddTaskFormComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-initiation/src/app/components/header/header.component.ts
+++ angular-initiation/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UiService } from 'src/app/services/ui.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,7 +11,7 @@ export class HeaderComponent implements OnInit {
   title: string = 'angular-initiation';
   showAddTask!: boolean;
   subscription!: Subscription;
-  constructor(private uiService: UiService) { 
+  constructor(private uiService: UiService, private router: Router) { 
     this.subscription = this.uiService.onToggle().subscribe((returnedState) => this.showAddTask = returnedState);
   }
 
@@ -22,6 +23,10 @@ export class HeaderComponent implements OnInit {
     // console.log("NEW TASK EMITTED");
     // this.uiService.onToggle().subscribe();
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route:string){
+    return this.router.url === route;
   }
 
 }
